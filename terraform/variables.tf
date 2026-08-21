2b3f4ab58cc48c1f685fc99cc9731156f51c03f3
praskumar005 <17404ec005@gmail.com>
2026-08-21 12:13:11 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "aws_region" {
+  description = "AWS region where resources will be provisioned"
+  default     = "us-east-1"
+}
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  default     = "ami-0b6d9d3d33ba97d99"
+}
+
+variable "instance_type" {
+  description = "Instance type for the EC2 instance"
+  default     = "t2.medium"
+}
+
+variable "my_enviroment" {
+  description = "Instance type for the EC2 instance"
+  default     = "dev"
+}
